6b162e1e89e135f4f7f6b97cdf8bc1fcc3341352
PatrickLyonsISD <patricklyonslit@gmail.com>
2023-11-12 12:40:45 +0000

    pulling down figma prototypes

--- app/src/main/java/com/example/TechTusQuiz/MainActivity.kt
+++ app/src/main/java/com/example/TechTusQuiz/MainActivity.kt
@@ -25,6 +25,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
 import com.example.TechTusQuiz.ui.GameScreen
 import com.example.TechTusQuiz.ui.theme.UnscrambleTheme
+import com.example.unscramble.frame2.Frame2
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,7 +37,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GameScreen()
+                    //GameScreen()
+                    Frame2()
                 }
             }
         }

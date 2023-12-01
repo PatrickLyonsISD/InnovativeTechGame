415f24508f79c2dbe2022a327bea4e35eeae964f
Patrick <patricklyonslit@gmail.com>
2023-12-01 20:29:17 +0000

    validation

--- app/src/main/java/com/example/TechTusQuiz/SignUp/SignUpviewModel.kt
+++ app/src/main/java/com/example/TechTusQuiz/SignUp/SignUpviewModel.kt
@@ -6,13 +6,29 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
 class SignUpViewModel : ViewModel() {
-    fun signUp(email: String, password: String, name: String,  signUpMessage: (String) -> Unit) {
+
+    fun signUp(email: String, password: String, kNumber: String, signUpMessage: (String) -> Unit) {
+        if (!isEmailValid(email)) {
+            signUpMessage("Invalid email address.")
+            return
+        }
+
+        if (password.length < 6) {
+            signUpMessage("Password must be at least 6 characters.")
+            return
+        }
+
+        if (!kNumber.startsWith("k", ignoreCase = true)) {
+            signUpMessage("K-number must start with 'k'.")
+            return
+        }
+
         val auth = FirebaseAuth.getInstance()
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
                     val userId = auth.currentUser?.uid
-                    addUserProfileToDatabase(userId, UserProfile(name))
+                    addUserProfileToDatabase(userId, UserProfile(kNumber))
                     signUpMessage("Sign Up Successful!")
                 } else {
                     signUpMessage("Sign Up Failed: ${task.exception?.message}")
@@ -25,11 +41,15 @@ class SignUpViewModel : ViewModel() {
             val databaseRef = FirebaseDatabase.getInstance().getReference("Users")
             databaseRef.child(uid).setValue(userProfile)
                 .addOnSuccessListener {
-
+                    // Handle success
                 }
                 .addOnFailureListener {
-
+                    // Handle failure
                 }
         }
+    }
+
+    private fun isEmailValid(email: String): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 }
